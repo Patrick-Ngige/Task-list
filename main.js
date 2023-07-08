4cb07ed9b2e73757d35774db3221bf8e0ee9b8a3
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-08 21:39:25 +0300

    submit button now working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
     const form = document.querySelector("#tk-form");
-    const input = document.querySelector("#tk-form-input");
+    const input = document.querySelector("#tk-form input");
     const list_el = document.querySelector("#tasks");
 
     form.addEventListener('submit', (e) => {
@@ -15,10 +15,11 @@ window.addEventListener('load', ()=> {
 
         const task_el = document.createElement("div");
         task_el.classList.add("task");
-        task_content_el.innerText = task;
+        
 
         const task_content_el = document.createElement("div");
-        task.task_content_el.classList.add("content");
+        task_content_el.classList.add("content");
+        task_content_el.innerText = task;
 
         task_el.appendChild(task_content_el);
 
